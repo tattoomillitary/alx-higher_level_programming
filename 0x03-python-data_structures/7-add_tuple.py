31bfe6d5e9d7e57cc7d617ab057ac1b30c9aa966
tattoomillitary <successbraimoh@gmail.com>
2023-01-08 06:45:16 -0800

    functional

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,7 +13,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1 = tuple_a[0]
         a2 = tuple_a[1]
 
-     if len_b == 0:
+    if len_b == 0:
         b1 = 0
         b2 = 0
     elif len_b == 1:
