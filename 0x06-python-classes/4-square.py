ae4720ee94ac4a9ae193a2ee37a657a06f860cfc
tattoomillitary <successbraimoh@gmail.com>
2023-01-24 12:30:04 -0800

    Trial fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
 
-    def size(self):
-        """ Method that returns the square are of the object
-        """
-        return (self.__size ** 2)
+        @property
+        def size(self):
+            """Get/set the current size of the square."""
+            return (self.__size)
 
-    @property
-    def size(self):
-        """ Method to returns the size value
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """ Method to set the size value of the square object
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
             self.__size = value
+
+        def area(self):
+            """Return the current area of the square."""
+            return (self.__size * self.__size)
